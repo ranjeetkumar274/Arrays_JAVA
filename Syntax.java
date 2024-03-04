1410372fa7a016655b639d13ef60f27614708f7d
ashuuydv <147934359+Ashuuydv@users.noreply.github.com>
2024-03-04 16:04:18 +0530

    first code of array

---
+++ Syntax.java
@@ -0,0 +1,14 @@
+public class Syntax {
+    public static void main(String[] args) {
+        int nums[] = new int[5];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+        nums[3] = 4;
+        nums[4] = 5;
+
+        System.out.println(nums[3]);
+
+
+    }
+}
